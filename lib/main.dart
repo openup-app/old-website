dae96b1489b4b2dfb20c63e28a32faa5fd59a4ea
Jaween Ediriweera <jaween.ediriweera@gmail.com>
2022-04-25 12:10:55 +0930

    Remove hash from page title

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:lottie/lottie.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
+  setUrlStrategy(PathUrlStrategy());
   runApp(const MyApp());
 }
 
